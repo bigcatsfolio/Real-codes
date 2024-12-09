994579842577e78a1bea53172170de8c87b6a573
bigcatsfolio <lipishagupta27@gmail.com>
2024-12-09 22:57:24 +0530

    Add files via upload

---
+++ reverse_array.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int a[n];
+    if(n<=0)
+    {
+        printf("Array Empty!!! or Invalid size");
+        return 0;
+    }
+
+    //Enter the elements of the array
+    printf("Enter the element: ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    //swapping in the same array
+    int temp; 
+    for(int j=0;j<n/2;j++)
+    {
+        temp=a[j];
+        a[j]=a[n-j-1];
+        a[n-j-1]=temp;
+    }
+
+    //printing the reversed array
+    for(int k=0;k<n;k++)
+    {
+        printf("%d ",a[k]);
+    }
+    return 0;
+}
